b349930cd9385f8d05b8a0b1f5323ea85c8593bb
Jose A. Hernandez <joseandresh2000@gmail.com>
2024-05-01 02:37:21 -0400

    Eliminando funciones reutilizadas

--- Curso_2/functions.php
+++ Curso_2/functions.php
@@ -6,21 +6,3 @@ function render_template(string $template, array $data = [])
   extract($data);
   require "templates/$template.php";
 }
-
-function get_data(string $url)
-{
-  $result = file_get_contents(API_URL);
-  $data = json_decode($result, true);
-  return $data;
-}
-
-function get_until_message(int $days): string
-{
-  return match (true) {
-    $days == 0 => "Se estrena hoy la película!",
-    $days == 1 => "Se estrena mañana la película!",
-    $days < 7 => "Se estrena esta semana la película!",
-    $days < 7 => "Se estrena este mes la película!",
-    default => "Faltan $days para la película",
-  };
-};
